323646747ac77e6d3896cbb31099d19bcf4deb19
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-02-10 11:14:33 -0800

    Updated middlewares, created action types

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,18 @@
-/* 
-  Action Types Go Here!
-  Be sure to export each action type so you can pull it into your reducer
-*/
+export const ADD_SMURF_FETCHING = "ADD_SMURF_START"
+export const ADD_SMURF_SUCCESS = "ADD_SMURF_SUCCESS"
+export const ADD_SMURF_FAILURE = "ADD_SMURF_FAILURE"
+
+export const DELETE_SMURF_FETCHING = "DELETE_SMURF_START"
+export const DELETE_SMURF_SUCCESS = "DELETE_SMURF_SUCCESS"
+export const DELETE_SMURF_FAILURE = "DELETE_SMURF_FAILURE"
+
+export const UPDATE_SMURF_FETCHING = "UPDATE_SMURF_START"
+export const UPDATE_SMURF_SUCCESS = "UPDATE_SMURF_SUCCESS"
+export const UPDATE_SMURF_FAILURE = "UPDATE_SMURF_FAILURE"
+
+export const GET_SMURFS_FETCHING = "GET_SMURFS_START"
+export const GET_SMURFS_SUCCESS = "GET_SMURFS_SUCCESS"
+export const GET_SMURFS_FAILURE = "GET_SMURFS_FAILURE"
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
